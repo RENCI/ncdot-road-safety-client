9ae65e2389b780ae175ea666e51e134945b58b6e
David <borland@renci.org>
2020-09-29 13:24:58 -0400

    Clean up webpack, remove index.html which is not used

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,6 @@
 const path = require('path');
 
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: __dirname + "/src/index.html",
-  filename: "index.html",
-  inject: "body"
-});
+
 
 module.exports = {
   entry: [
@@ -30,6 +25,5 @@ module.exports = {
         loader: "babel-loader"
       }
     ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
+  }
 };
