31c9bf2f65c51c66f89e892393ac50d4777763e3
David <borland@renci.org>
2020-10-16 10:27:00 -0400

    Reorganize code for browser view

--- src/components/scene/scene.js
+++ src/components/scene/scene.js
@@ -6,9 +6,9 @@ import './scene.css'
 export const Scene = ({ timestamp }) => {
   return (
     <div className="scene">
-      <img src={ api.getImage(timestamp, 'right') } width="340" />
-      <img src={ api.getImage(timestamp, 'front') } width="340" />
-      <img src={ api.getImage(timestamp, 'left') } width="340" />
+      <img src={ api.getImage(timestamp, 'right') } width="33%" />
+      <img src={ api.getImage(timestamp, 'front') } width="33%" />
+      <img src={ api.getImage(timestamp, 'left') } width="33%" />
     </div>
   )
 }
