066108361e7067cd73b8dccaa2601f00ad2a3445
Matt Watson <mattwat@gmail.com>
2021-01-14 11:37:59 -0500

    add basic route browse view

--- src/views/browse-route.js
+++ src/views/browse-route.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import { Typography } from 'antd'
-import { RouteBrowser } from '../components/route-browser'
+import React, { Fragment, useContext, useEffect, useMemo, useRef, useState } from 'react'
+import PropTypes from 'prop-types'
+import { Link, useHistory, useParams } from 'react-router-dom'
+import { api } from '../api'
+import { Button, InputNumber, Slider, Tooltip, Typography } from 'antd'
+import { FastBackwardOutlined, StepBackwardOutlined, StepForwardOutlined, FastForwardOutlined } from '@ant-design/icons'
+import { Scene } from '../components/scene'
+import { loadModules } from 'esri-loader'
+import { Map as EsriMap } from '@esri/react-arcgis'
+import { useRouteBrowserContext } from '../contexts'
 
-const { Title } = Typography
+const { Paragraph, Title } = Typography
+
+// main view
 
 export const BrowseRouteView = () => {
-  return (
-    <>
-      <Title level={ 1 }>Browse by Route</Title>
 
-      <RouteBrowser />
-    </>
+  return (
+    <div>
+      <Title level={ 1 }>Browsing route</Title>
+    </div>
   )
 }
+
+      // points={ [[currentLocation.lat, currentLocation.long]] } />
