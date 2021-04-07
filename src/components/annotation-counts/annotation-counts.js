75b4c3e82ab3bebc80c8f20926b584c597bbab40
David Borland <borland@renci.org>
2021-04-07 14:22:13 -0400

    Feature/webpack update (#87)
    * Updating webpack to latest v4
    
    * Update browserslist
    
    * Update to webpack 5.3

--- src/components/annotation-counts/annotation-counts.js
+++ src/components/annotation-counts/annotation-counts.js
@@ -33,7 +33,7 @@ export const AnnotationSummary = ({ annotationName }) => {
 
   return (
     <Space direction="vertical" style={{ width: '100%' }}>
-      <Collapse collapsible="header">
+      <Collapse>
         <Panel header="Annotation Summary" key="1">
           You have saved <strong>{ savedImages.length }</strong> images during this session.<br/>
           You have annotated <strong>{ counts.annotations }</strong> { annotationName }.<br/>
