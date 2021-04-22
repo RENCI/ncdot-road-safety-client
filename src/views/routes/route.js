65779a96d4e704c5773bead4b6920f172d643191
Matt Watson <mattwat@gmail.com>
2021-04-22 18:23:30 -0400

    grab image base names for given route

--- src/views/routes/route.js
+++ src/views/routes/route.js
@@ -11,12 +11,15 @@ export const BrowseRouteView = (props) => {
   // note that imageIndex is shifted by one for human readability
   const { routeID, imageIndex = 1 } = useParams()
   const [imageIDs, setImageIDs] = useState([])
+  const [currentLocation, setCurrentLocation] = useState({})
 
   console.log({ routeID, imageIndex })
 
+  // grab the image base names for all scenes along this route
   useEffect(() => {
     const fetchRouteImageBaseNames = async () => await api.getRouteInfo(routeID)
       .then(response => {
+        console.log(response)
         if (response?.data?.route_image_base_names) {
           setImageIDs(response.data.route_image_base_names)
         }
@@ -25,9 +28,23 @@ export const BrowseRouteView = (props) => {
     fetchRouteImageBaseNames()
   }, [])
 
-  // when image (index) changes,
+  // when scene/location changes,
   // update the document title with route & image info
   useEffect(() => { document.title = `Route ${ routeID }, #${ imageIndex } | RHF` }, [routeID, imageIndex])
+
+  // when scene/location changes,
+  // fetch metadata for the current location's scene
+  useEffect(() => {
+    const fetchSceneMetadata = async () => await api.getImageMetadata(imageIDs[index])
+      .then(response => {
+        console.log(response)
+        setCurrentLocation(response.data.metadata)
+      })
+      .catch(error => console.error(error))
+    if (imageIDs.length && index) {
+      fetchSceneMetadata()
+    }
+  }, [routeID, imageIndex])
 
   return (
     <Fragment>
@@ -41,6 +58,9 @@ export const BrowseRouteView = (props) => {
         </Breadcrumb.Item>
         <Breadcrumb.Item>{ imageIndex }</Breadcrumb.Item>
       </Breadcrumb>
+      <pre>
+        { JSON.stringify(imageIDs, null, 2) }
+      </pre>
     </Fragment>
   )
 }
