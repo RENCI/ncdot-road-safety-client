7448f8a444d528524ec683be20d713e159c91cfa
Matt Watson <mattwat@gmail.com>
2021-04-30 11:28:58 -0400

    add ability to select base map (#99)

--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,22 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Map as EsriMap } from '@esri/react-arcgis'
+import { Select } from 'antd';
 import { Marker } from './marker'
+import './map.css'
+
+const { Option } = Select
 
 const ncCenter = { lat: 35.393809, long: -79.8431 }
 
-export const Map = ({ height, markers }) => {
+export const Map = ({ height, markers, basemapSelection = true }) => {
+  const [basemap, setBasemap] = useState('gray-vector')
+
+  const handleChangeBasemap = value => {
+    setBasemap(value)
+  }
+
   return (
-    <div className="route-table-map">
+    <div className="map">
+
       <EsriMap
+        key={ basemap}
         style={{ overflow: 'hidden', height: height }}
-        mapProperties={{ basemap: 'gray-vector' }}
+        mapProperties={{ basemap: basemap }}
         viewProperties={{
           center: [ncCenter.long, ncCenter.lat],
           zoom: 7
       }}>
         { markers.length > 0 && markers.map(({ key, ...props }, i) => <Marker key={ `marker-${ i }_-${ props.long },${ props.lat }` } { ...props } />) }
       </EsriMap>
+      {
+        basemapSelection && (
+          <Select className="basemap-selector" defaultValue="gray-vector" onChange={ handleChangeBasemap }>
+            <Option value="dark-gray-vector">Dark Gray Canvas</Option>
+            <Option value="gray-vector">Light Gray Canvas</Option>
+            <Option value="osm">OpenStreetMap</Option>
+            <Option value="satellite">Satellite</Option>
+            <Option value="streets">Streets</Option>
+            <Option value="topo">Topographic Map</Option>
+            <Option value="streets-navigation-vector">World Navigation Map</Option>
+            <Option value="streets-night-vector">World Street Map (Night)</Option>
+            <Option value="streets-relief-vector">World Street Map (with Relief)</Option>
+          </Select>
+        )
+      }
+
     </div>
   )
 }
