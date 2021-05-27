c9b1eaaac9a88b4dacb04327afc0d0b0d14e5143
Matt Watson <mattwat@gmail.com>
2021-05-27 15:17:32 -0400

    make route and image ids copyable

--- src/components/route-browser/scene-metadata.js
+++ src/components/route-browser/scene-metadata.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Typography } from 'antd'
 import { useRouteBrowseContext } from './context'
 
-const { Paragraph } = Typography
+const { Paragraph, Text } = Typography
 
 export const SceneMetadata = () => {
   const { currentLocation, imageIDs, index, routeID } = useRouteBrowseContext()
@@ -10,8 +10,8 @@ export const SceneMetadata = () => {
     <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end', fontSize: '80%', opacity: 0.75 }}>
       <Paragraph style={{ textAlign: 'left' }}>
         { index + 1 } of { imageIDs.length }<br />
-        Image ID: { imageIDs[index] || '...' } <br />
-        Route ID: { routeID || '...' } <br />
+        Image ID: <Text copyable>{ imageIDs[index] || '...' }</Text> <br />
+        Route ID: <Text copyable>{ routeID || '...' }</Text> <br />
       </Paragraph>
       <Paragraph style={{ textAlign: 'right' }}>
         Latitude: { currentLocation.lat || '...' }<br />
