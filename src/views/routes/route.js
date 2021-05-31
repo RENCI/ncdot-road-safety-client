b4ff0079f98ebed28de6d9f58d3faa4a1f3a567a
Matt Watson <mattwat@gmail.com>
2021-05-31 00:31:52 -0400

    add separate route overview view

--- src/views/routes/route.js
+++ src/views/routes/route.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useContext, useEffect, useMemo, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { api } from '../../api'
 import { Col, Divider, Row, Space, Typography } from 'antd'
@@ -14,7 +14,7 @@ import {
   ScenePrefetch,
 } from '../../components/route-browser'
 import { Map } from '../../components/map'
-const { Title } = Typography
+const { Text, Title } = Typography
 
 export const BrowseRouteView = () => {
   // grab parameters passed in from the route /routes/:routeID/:imageIndex
@@ -27,12 +27,12 @@ export const BrowseRouteView = () => {
   useEffect(() => {
     // use index 0...
     // ...if no imageIndex
-    if (!imageIndex) { setIndex(0); return; }
+    if (!imageIndex) { setIndex(-1); return; }
     // ...if no route or no images on route
-    if (!routeID || imageIDs.length === 0) { setIndex(0); return; }
+    if (!routeID || imageIDs.length === 0) { setIndex(-1); return; }
     const i = parseInt(imageIndex) - 1
     // ...if index lies outside 0..(# of images - 1)
-    if (i < 0 || imageIDs.length < i + 1) { setIndex(0); return; }
+    if (i < 0 || imageIDs.length < i + 1) { setIndex(-1); return; }
     // ok, let's go
     setIndex(i)
   }, [imageIndex, imageIDs.length])
@@ -69,44 +69,74 @@ export const BrowseRouteView = () => {
     <RouteBrowser value={{ routeID, imageIDs, index, imageIndex, currentLocation }}>
       <Title level={ 1 }>Route { routeID }</Title>
 
-      <Breadcrumbs crumbs={[
-        { text: 'Home', path: '/' },
-        { text: routeID, path: `/routes/${ routeID }` },
-        { text: index + 1, path: `/routes/${ routeID }/${ imageIndex }` },
-      ]} />
+      {
+        index === -1 ? (
+          <Fragment>
+            <Breadcrumbs crumbs={[
+              { text: 'Home', path: '/' },
+              { text: 'Routes', path: `/routes` },
+              { text: routeID, path: `/routes/${ routeID }` },
+            ]} />
 
-      <Divider />
+            <Divider />
 
-      <PredictionsGraph />
-      <RouteNavigation />
+            <Link to={ `/routes/${ routeID }/1` }>Browse this Route</Link>
 
-      <br />
+            <Text>{ imageIDs.length } images</Text>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <Breadcrumbs crumbs={[
+              { text: 'Home', path: '/' },
+              { text: 'Routes', path: `/routes` },
+              { text: routeID, path: `/routes/${ routeID }` },
+              { text: index + 1, path: `/routes/${ routeID }/${ imageIndex }` },
+            ]} />
 
-      <Scene id={ imageIDs[index] } />
-      <SceneMetadata />
+            <Divider />
 
-      <Divider />
+            <PredictionsGraph />
+            <RouteNavigation />
 
-      <Row gutter={ 32 }>
-        <Col md={ 24 } lg={ 18 }>
-          <Map markers={ [currentLocation] } height="400px" zoom={ 13 } />
-          <br />
-        </Col>
-        <Col md={ 24 } lg={ 6 }>
-          <Title level={ 2 }>Feature Predictions</Title>
-          <PredictionsList key={ currentLocation.id } />
-        </Col>
-      </Row>
+            <br />
 
-      { // scene back ten steps 
-        0 < imageIDs.length && 0 < index - 9 && <ScenePrefetch id={ imageIDs[index - 10] } /> }
-      { // prev scene
-        0 < imageIDs.length && 0 <= index - 1 && <ScenePrefetch id={ imageIDs[index - 1] } /> }
-      { // next scene
-        0 < imageIDs.length && index + 1 < imageIDs.length && <ScenePrefetch id={ imageIDs[index + 1] } /> }
-      { // scene forward ten steps 
-        0 < imageIDs.length && index + 10 < imageIDs.length && <ScenePrefetch id={ imageIDs[index + 10] } /> }
+            <Scene id={ imageIDs[index] } />
+            <SceneMetadata />
+
+            <Divider />
+
+            <Row gutter={ 32 }>
+              <Col md={ 24 } lg={ 18 }>
+                <Map markers={ [currentLocation] } height="400px" zoom={ 13 } />
+                <br />
+              </Col>
+              <Col md={ 24 } lg={ 6 }>
+                <Title level={ 2 }>Feature Predictions</Title>
+                <PredictionsList key={ currentLocation.id } />
+              </Col>
+            </Row>
+
+            { // scene back ten steps 
+              0 < imageIDs.length && 0 < index - 9 && <ScenePrefetch id={ imageIDs[index - 10] } /> }
+            { // prev scene
+              0 < imageIDs.length && 0 <= index - 1 && <ScenePrefetch id={ imageIDs[index - 1] } /> }
+            { // next scene
+              0 < imageIDs.length && index + 1 < imageIDs.length && <ScenePrefetch id={ imageIDs[index + 1] } /> }
+            { // scene forward ten steps 
+              0 < imageIDs.length && index + 10 < imageIDs.length && <ScenePrefetch id={ imageIDs[index + 10] } /> }
+          </Fragment>          
+        )
+      }
+
 
     </RouteBrowser>
+  )
+}
+
+const RouteOverview = () => {
+  return (
+    <div>
+      Route 
+    </div>
   )
 }
