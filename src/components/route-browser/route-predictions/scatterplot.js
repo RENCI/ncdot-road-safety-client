d615c606e691cbca2b0c3799f242de4d7c9131e9
Matt Watson <mattwat@gmail.com>
2021-07-08 13:53:48 -0400

    add zoom functionality to predictions scatterplot

--- src/components/route-browser/route-predictions/scatterplot.js
+++ src/components/route-browser/route-predictions/scatterplot.js
@@ -1,13 +1,14 @@
 import React, { Fragment, useEffect, useMemo, useState } from 'react'
 import { useHistory } from 'react-router-dom'
-import { Card, Col, Row, Select, Space, Typography } from 'antd'
+import { Button, Card, Col, Row, Select, Space, Typography } from 'antd'
+import { ZoomInOutlined as ZoomInIcon, ZoomOutOutlined as ZoomOutIcon } from '@ant-design/icons'
 import { useRouteContext } from '../context'
 import { api } from '../../../api'
 import { ResponsiveScatterPlot } from '@nivo/scatterplot'
 import { GraphTooltip, Legend, ScatterplotPoint } from './'
 import './scatterplot.css'
 
-const { Text } = Typography
+const { Text, Title } = Typography
 
 const features = ['guardrail', 'pole']
 
@@ -47,7 +48,7 @@ const ThresholdLineLayer = props => {
   )
 }
 
-const Graph = ({ data, predictionThreshold }) => {
+const Graph = ({ data, min, max, predictionThreshold }) => {
   const history = useHistory()
   const { currentLocation, images, routeID } = useRouteContext()
   
@@ -63,7 +64,7 @@ const Graph = ({ data, predictionThreshold }) => {
         data={ data }
         height={ 175 }
         margin={{ top: 0, right: 8, bottom: 0, left: 40 }}
-        xScale={{ type: 'linear', min: 1, max: images.length }}
+        xScale={{ type: 'linear', min: min, max: max }}
         yScale={{ type: 'linear', min: -0.05, max: 1.05, stacked: false, reverse: false }}
         yFormat=" >-.2f"
         enableGridX={ true }
@@ -108,12 +109,37 @@ const Graph = ({ data, predictionThreshold }) => {
 }
 
 export const PredictionsScatterplot = ({ key }) => {
-  const { images } = useRouteContext()
+  const { images, index } = useRouteContext()
   const [predictions, setPredictions] = useState([])
   const [selectedFeature, setSelectedFeature] = useState('guardrail')
   const [threshold, setThreshold] = useState()
-
+  const [zoom, setZoom] = useState(1)
   const handleFeatureSelect = value => setSelectedFeature(value)
+  const handleZoomSelect = value => setZoom(value)
+
+  const extrema = useMemo(() => {
+    if (zoom === 1) {
+      return {
+        min: 1,
+        max: images.length,
+      }
+    }
+    const zoomRadius = Math.ceil(images.length / (2 * zoom))
+    let min = Math.max(0, index - zoomRadius)
+    let max = Math.min(images.length, index + zoomRadius)
+    if (min < 1) {
+      min = 1
+      max = 2 * zoomRadius
+    }
+    if (max > images.length) {
+      min -= images.length - 2 * zoomRadius
+      max = images.length
+    }
+    return {
+      min: min,
+      max: max,
+    }
+  }, [index, zoom])
 
   // massage the prediction data into a format usable by this Nivo graph component.
   useEffect(() => {
@@ -148,11 +174,14 @@ export const PredictionsScatterplot = ({ key }) => {
   return (
     <Row gutter={ 32 }>
       <Col xs={ 24 } lg={ 18 }>
-        <Graph data={ [predictions[selectedFeature]] } predictionThreshold={ threshold } />
+        <Graph data={ [predictions[selectedFeature]] } predictionThreshold={ threshold } min={ extrema.min } max={ extrema.max } />
       </Col>
       <Col xs={ 24 } lg={ 6 }>
         <Select value={ selectedFeature } onChange={ handleFeatureSelect } style={{ width: '100%' }}>
           { features.map(feature => <Select.Option key={ `feature-option-${ feature }` } value={ feature }>{ feature }</Select.Option>) }
+        </Select>
+        <Select value={ zoom } onChange={ handleZoomSelect } style={{ width: '100%' }}>
+          { [1, 2, 3, 4, 5].map(level => <Select.Option key={ `zoom-option-${ level }x` } value={ level }>{ `Zoom ${ level }x` }</Select.Option>) }
         </Select>
         <Legend />
       </Col>
