482126ccdd2196c29f9b42a82b5355b028aa1c33
Matt Watson <mattwat@gmail.com>
2021-07-20 11:14:01 -0400

    don't pass array of undefineds for markers

--- src/views/routes/route-summary.js
+++ src/views/routes/route-summary.js
@@ -30,8 +30,8 @@ const initialAnnotationCounts = ['guardrail', 'pole'].reduce((obj, feature) => (
 export const RouteSummaryView = () => {
   const history = useHistory()
   const { routeID, routeLength, images } = useRouteContext()
-  const [startingCoordinates, setStartingCoordinates] = useState({ lat: 0, long: 0 })
-  const [endingCoordinates, setEndingCoordinates] = useState({ lat: 0, long: 0 })
+  const [startingCoordinates, setStartingCoordinates] = useState()
+  const [endingCoordinates, setEndingCoordinates] = useState()
   const [pathCoordinates, setPathCoordinates] = useState([])
   const [annotationCounts, setAnnotationCounts] = useState()
 
@@ -133,7 +133,7 @@ export const RouteSummaryView = () => {
       <Divider orientation="center">Map</Divider>
 
       <Map
-        markers={ [startingCoordinates, endingCoordinates] }
+        markers={ startingCoordinates && endingCoordinates ? [startingCoordinates, endingCoordinates] : [] }
         path={ pathCoordinates }
         height="600px"
         zoom={ 13 }
